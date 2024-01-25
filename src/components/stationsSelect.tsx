1c7249b955f8888afbf0116d589d38d224fa1cd5
SirBerg <benno@trellis.de>
2024-01-25 18:13:23 +0100

    Ran file through prettier

--- src/components/stationsSelect.tsx
+++ src/components/stationsSelect.tsx
@@ -28,8 +28,8 @@ type Props = {
   onSelect: (station: Station) => void;
 };
 
-export function StationSelect(props: Props):React.JSX.Element {
-  const maxStationsInitialLoad:number = 500
+export function StationSelect(props: Props): React.JSX.Element {
+  const maxStationsInitialLoad: number = 500;
   const {
     availableStations: availableStations,
     selectedStations: selectedStations,
@@ -39,16 +39,25 @@ export function StationSelect(props: Props):React.JSX.Element {
     selectedStations: Station[];
     onSelect: (station: Station) => void;
   } = props;
-  const [open, setOpen]: [open: boolean, setOpen: React.Dispatch<React.SetStateAction<boolean>>] = useState(false);
-  const [allStationsLoaded, setAllStationsLoaded] : [allStationsLoaded: boolean, setAllStationsLoaded: React.Dispatch<boolean>] = useState(false)
-  const [stations, setStations] : [stations: Array<Station>, setStation: React.Dispatch<React.SetStateAction<Array<Station>>>] = useState(availableStations.slice(0, maxStationsInitialLoad))
+  const [open, setOpen]: [
+    open: boolean,
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
+  ] = useState(false);
+  const [allStationsLoaded, setAllStationsLoaded]: [
+    allStationsLoaded: boolean,
+    setAllStationsLoaded: React.Dispatch<boolean>
+  ] = useState(false);
+  const [stations, setStations]: [
+    stations: Array<Station>,
+    setStation: React.Dispatch<React.SetStateAction<Array<Station>>>
+  ] = useState(availableStations.slice(0, maxStationsInitialLoad));
 
   //runs everytime the dropdown is opened
-  React.useEffect(():void => {
-    if(!availableStations) return
-    setAllStationsLoaded(false)
-    setStations(availableStations.slice(0, maxStationsInitialLoad))
-  }, [open])
+  React.useEffect((): void => {
+    if (!availableStations) return;
+    setAllStationsLoaded(false);
+    setStations(availableStations.slice(0, maxStationsInitialLoad));
+  }, [open]);
 
   let boxLabel: string;
   if (selectedStations.length === 0) {
@@ -61,71 +70,77 @@ export function StationSelect(props: Props):React.JSX.Element {
 
   return (
     <>
-        <>
-            <Popover open={open} onOpenChange={setOpen}>
-              <PopoverTrigger asChild>
-                <Button
-                  variant="outline"
-                  role="combobox"
-                  aria-expanded={open}
-                  className="w-[200px] justify-between"
-                >
-                  {boxLabel}
-                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-[200px] p-0">
-                <Command>
-                  <CommandInput placeholder="Search station..." />
-                  <ScrollArea className="h-96">
-                    {
-                      allStationsLoaded ? <></> :
-                      <Button
-                        variant="outline"
-                        className="w-full justify-center"
-                        onClick={() => {
-                          setStations(availableStations)
-                          setAllStationsLoaded(true)
-                        }}>
-                          Load All Stations...
-                      </Button>
-                    }
-                    <CommandEmpty>No station found.</CommandEmpty>
-                    <CommandGroup>
-                      {stations.map((station: Station) => (
-                          <CommandItem
-                              key={station.id}
-                              value={station.name}
-                              onSelect={(currentValue: string): void => {
-                                const selectedStation: Station | undefined = availableStations ? availableStations.find(
-                                    (station: Station): boolean =>
-                                        station.name.toLowerCase() === currentValue,
-                                ) : undefined;
+      <>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="w-[200px] justify-between"
+            >
+              {boxLabel}
+              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0">
+            <Command>
+              <CommandInput placeholder="Search station..." />
+              <ScrollArea className="h-96">
+                {allStationsLoaded ? (
+                  <></>
+                ) : (
+                  <Button
+                    variant="outline"
+                    className="w-full justify-center"
+                    onClick={() => {
+                      setStations(availableStations);
+                      setAllStationsLoaded(true);
+                    }}
+                  >
+                    Load All Stations...
+                  </Button>
+                )}
+                <CommandEmpty>No station found.</CommandEmpty>
+                <CommandGroup>
+                  {stations.map((station: Station) => (
+                    <CommandItem
+                      key={station.id}
+                      value={station.name}
+                      onSelect={(currentValue: string): void => {
+                        const selectedStation: Station | undefined =
+                          availableStations
+                            ? availableStations.find(
+                                (station: Station): boolean =>
+                                  station.name.toLowerCase() === currentValue
+                              )
+                            : undefined;
 
-                                setOpen(false);
-                                setAllStationsLoaded(false)
-                                if (selectedStation) onSelect(selectedStation);
-                              }}
-                          >
-                            <Check
-                                className={cn(
-                                    "mr-2 h-4 w-4",
-                                    selectedStations.find(
-                                        (selected: Station): boolean => selected.id == station.id,
-                                    )
-                                        ? "opacity-100"
-                                        : "opacity-0",
-                                )}
-                            />
-                            {station.name}
-                          </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </ScrollArea>
-                </Command>
-              </PopoverContent>
-            </Popover>
-          </>
+                        setOpen(false);
+                        setAllStationsLoaded(false);
+                        if (selectedStation) onSelect(selectedStation);
+                      }}
+                    >
+                      <Check
+                        className={cn(
+                          "mr-2 h-4 w-4",
+                          selectedStations.find(
+                            (selected: Station): boolean =>
+                              selected.id == station.id
+                          )
+                            ? "opacity-100"
+                            : "opacity-0"
+                        )}
+                      />
+                      {station.name}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </ScrollArea>
+            </Command>
+          </PopoverContent>
+        </Popover>
       </>
+    </>
   );
 }
