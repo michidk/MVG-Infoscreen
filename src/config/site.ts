889d60328eb6d34245a19904886f03d4034ac37f
Michael Lohr <michael@lohr.dev>
2024-02-21 09:13:37 +0100

    fix: Fix the url in the station selector (#20)

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,17 +1,21 @@
 export type SiteConfig = typeof siteConfig;
 
-function urlify(url: string | undefined): string | undefined {
-  if (!url) {
-    return undefined;
-  }
-  if (url.startsWith("http") || url.startsWith("https")) {
-    return url;
-  }
-  return `https://${url}`;
+function urlify(url: string): string {
+	if (url.startsWith("http") || url.startsWith("https")) {
+		return url;
+	}
+	return `https://${url}`;
 }
 
 export function getServerUrl(): string {
-  return urlify(process.env.NEXT_PUBLIC_VERCEL_URL) || urlify(process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL) || process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
+	return process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+		? "https://mvg-infoscreen.vercel.app"
+		: urlify(
+				process.env.NEXT_PUBLIC_VERCEL_URL ||
+					process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ||
+					process.env.NEXT_PUBLIC_URL ||
+					"http://localhost:3000",
+		  );
 }
 
 export const siteConfig = {
